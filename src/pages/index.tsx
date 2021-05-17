0ad111fa56f1c62a95cce2f61fb07501ca02ae76
Everton Oliveira <everton.belem@hotmail.com>
2021-05-17 13:44:50 -0300

    eslint and prettier

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,31 +1,34 @@
-import React, { useCallback } from 'react';
-import { GetStaticProps } from 'next';
-import Head from 'next/head';
-import axios from 'axios';
-import { GoogleMap, useJsApiLoader, LoadScript, MarkerClusterer, Marker } from '@react-google-maps/api';
+import React from 'react'
+import { GetStaticProps } from 'next'
+import axios from 'axios'
+import { GoogleMap, useJsApiLoader, MarkerClusterer, Marker } from '@react-google-maps/api'
 
+import styles from './home.module.scss'
 
 interface HomeProps {
-  formated: [{
-    lat: number;
-    lng: number;
-    name: string;
-  }];
+  formated: [
+    {
+      lat: number
+      lng: number
+      name: string
+    }
+  ]
 }
 
 const containerStyle = {
   width: '100%',
-  height: '850px'
-};
+  height: '400px',
+}
 
 const center = {
   lat: -23.784721661904463,
-  lng: -46.00195680244929
-};
+  lng: -46.00195680244929,
+}
 
 export default function Home({ formated }: HomeProps) {
+  const googleMapsApiKey: any = process.env.mapsKey
   const { isLoaded } = useJsApiLoader({
-    googleMapsApiKey: process.env.mapsKey
+    googleMapsApiKey,
   })
 
   const options = {
@@ -33,45 +36,57 @@ export default function Home({ formated }: HomeProps) {
       'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m',
   }
 
-  function createKey(location) {
+  function createKey(location: { lat: number; lng: number; name?: string }) {
     return location.lat + location.lng
   }
-  
-  return (
-    isLoaded ? (
-      <GoogleMap
-        mapContainerStyle={containerStyle}
-        center={center}
-        zoom={3}
-      >
-        <MarkerClusterer options={options}>
-            {(clusterer) =>
-              formated.map((location) => (
-                <Marker key={createKey(location)} position={location} clusterer={clusterer} />
-              ))
-            }
-          </MarkerClusterer>
-        <></>
-      </GoogleMap>
-    ) : 
+
+  return isLoaded ? (
+    <div className={styles.contentContainer}>
+      <>
+        {formated.length ? (
+          <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={3}>
+            <MarkerClusterer options={options}>
+              {(clusterer) =>
+                formated.map((location) => (
+                  <Marker key={createKey(location)} position={location} clusterer={clusterer} />
+                ))
+              }
+            </MarkerClusterer>
+          </GoogleMap>
+        ) : (
+          <div>
+            <h1>carregando...</h1>
+          </div>
+        )}
+      </>
+    </div>
+  ) : (
     <></>
-  );
+  )
 }
 
 export const getStaticProps: GetStaticProps = async () => {
-  const { data } = await axios.get('http://images.contelege.com.br/poi.json');
- 
-  const formated = data.map((item) => {
-    return {
-      lat: item.latitude,
-      lng: item.longitude,
-      name: item.name
-    }
-  })
+  const { data } = await axios.get('http://images.contelege.com.br/poi.json')
 
-  return {
-    props: {
-      formated
+  if (data) {
+    const formated = data.map((item: { latitude: number; longitude: number; name: string }) => {
+      return {
+        lat: item.latitude,
+        lng: item.longitude,
+        name: item.name,
+      }
+    })
+
+    return {
+      props: {
+        formated,
+      },
+    }
+  } else {
+    return {
+      props: {
+        formated: [],
+      },
     }
   }
 }
